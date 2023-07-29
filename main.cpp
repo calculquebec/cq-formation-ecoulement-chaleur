1ebb2ccefc41a813ca3a34ca26faa1566edcf681
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2023-07-29 17:23:42 -0400

    Utiliser des doubles pour le modèle

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <cmath>
 #include <cstring>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <vector>
 #include <png.h>
@@ -12,52 +13,43 @@ const unsigned int NB_MAX_ITER = 4096;
 
 
 /**
- * Classe facilitant la lecture-écriture (Le) de fichiers PNG
+ * Classe facilitant la lecture-écriture (Le) de fichiers PNG en RGB
  */
-class LePNG
+class LePNG: public std::vector<png_color>
 {
 public:
     LePNG() {
         memset(&entete, 0, sizeof entete);
+
+        entete.format = PNG_FORMAT_RGB;
         entete.version = PNG_IMAGE_VERSION;
     }
 
-    ~LePNG() {
+    virtual ~LePNG() {
         png_image_free(&entete);
+    }
+
+    /**
+     * Charger une image d'un fichier PNG - Triple canal (Red, Green, Blue)
+     */
+    void charger(const std::string & nom_fichier) {
+        if (!png_image_begin_read_from_file(&entete, nom_fichier.c_str()))
+            throw nom_fichier + " - " + entete.message;
+
+        resize(entete.width * entete.height);
+
+        if (!png_image_finish_read(&entete, NULL, data(), 0, NULL))
+            throw nom_fichier + " - " + entete.message;
     }
 
     /**
      * Enregistrer le résultat dans un fichier PNG
      */
-    void enregistrer(const png_bytep data, const std::string & nom_fichier) {
-        if (data == NULL)
-            throw "le vecteur de données pointe vers NULL";
-
+    void enregistrer(const std::string & nom_fichier) {
         if (!png_image_write_to_file(
-            &entete, nom_fichier.c_str(), 0, data, 0, NULL)) {
+                &entete, nom_fichier.c_str(), 0, data(), 0, NULL)) {
             throw nom_fichier + " - " + entete.message;
         }
-    }
-
-    /**
-     * Chargement des métadonnées du fichier PNG
-     */
-    void init_lecture(const std::string & nom_fichier) {
-        if (!png_image_begin_read_from_file(&entete, nom_fichier.c_str()))
-            throw nom_fichier + " - " + entete.message;
-    }
-
-    /**
-     * Lecture des pixels RGB (rouge, vert, bleu), 3x8 bits
-     */
-    void lire_RGB(png_bytep data) {
-        if (data == NULL)
-            throw "le vecteur de données pointe vers NULL";
-
-        entete.format = PNG_FORMAT_RGB;
-
-        if (!png_image_finish_read(&entete, NULL, data, 0, NULL))
-            throw entete.message;
     }
 
     inline png_uint_32 largeur() const { return entete.width; }
@@ -69,12 +61,50 @@ private:
 
 
 /**
- * Triplet (Chaleur, Température, Conduction), 8 bits par composante
+ * Affiche un aperçu du modèle en ASCII
+ * @param ostr   Objet std::ostream tel que std::cout
+ * @param modele Le modèle chaleur-température-conduction
+ * @return L'objet std::ostream pour des appels à la chaîne
+ */
+std::ostream& operator<<(std::ostream & ostr, const LePNG & png)
+{
+    const char ascii[] = " .,!~:;+=xzXZ%#@";
+    const auto largeur = png.largeur(), hauteur = png.hauteur();
+    const auto sautH = png.largeur() / 80;  // caractères de large
+    const auto sautV = sautH * 2;
+
+    ostr << "Taille du modèle: " << largeur << " x " << hauteur << std::endl;
+
+    // Pour chaque bloc complet de taille sautH x sautV
+    for (auto i = sautV + hauteur % sautV / 2; i < hauteur; i += sautV) {
+        for (auto j = sautH + largeur % sautH / 2; j < largeur; j += sautH) {
+            unsigned int temp_tot = 0;
+
+            // Calculer la moyenne des valeurs de conduction ( [0, 256[ )
+            for (auto ii = i - sautV; ii < i; ++ii) {
+                for (auto jj = j - sautH; jj < j; ++jj) {
+                    temp_tot += png[ii * largeur + jj].green;
+                }
+            }
+
+            // Sélectionner un des 16 caractères ASCII (/16 = *16/256)
+            ostr << ascii[temp_tot / (sautV * sautH * 16)];
+        }
+
+        ostr << std::endl;
+    }
+
+    return ostr;
+}
+
+
+/**
+ * Triplet (Chaleur, Température, Conduction)
  */
 typedef struct {
-    png_byte chal;  // Intensité de la source de chaleur
-    png_byte temp;  // En degrés Celsius
-    png_byte cond;  // Facteur de conduction de chaleur
+    double chaleur;      // Intensité de la source de chaleur
+    double temperature;  // Température en degrés Celsius
+    double conduction;   // Facteur de conduction de chaleur
 } CTC;
 
 
@@ -86,51 +116,14 @@ class ModeleCTC: public std::vector<CTC>
 public:
     ModeleCTC(): larg(0), haut(0) {}
 
-    void enregistrer(LePNG & png, const std::string & nom_fichier) const {
-        png.enregistrer((const png_bytep)data(), nom_fichier);
-    }
-
     /**
-     * Redimensionner la grille et lire les données du fichier PNG
+     * Redimensionner la grille
      */
-    void initialiser(LePNG & png) {
-        larg = png.largeur();
-        haut = png.hauteur();
+    void redimensionner(std::size_t largeur, std::size_t hauteur) {
+        larg = largeur;
+        haut = hauteur;
 
         resize(larg * haut);
-        png.lire_RGB((png_bytep)data());
-    }
-
-    double un_pas_de_temps() {
-        double delta_temp = 0.;
-
-        // Convergence accélérée si traité en damier (une couleur à la fois)
-        for (auto impair = 0; impair < 2; ++impair) {
-            // Laisser faire la marge de 1 pixel
-            for (auto i = 1; i < haut - 1; ++i) {
-                auto depart = (((i + 1) ^ impair) & 1);  // Damier
-
-                for (auto j = 1 + depart; j < larg - 1; j += 2) {
-                    int conduct = conduction(i, j);
-                    int ancienne_temp = temperat(i, j);
-                    int nouvelle_temp = ((int)
-                        temperat(i - 1, j) +
-                        temperat(i, j - 1) +
-                        temperat(i, j + 1) +
-                        temperat(i + 1, j) + 3) / 4;
-
-                    png_byte temp_finale = (
-                        (256 - conduct) * ancienne_temp +
-                        conduct * nouvelle_temp + 128) / 256;
-                    ctc(i, j).temp = std::max(temp_finale, chaleur(i,j));
-
-                    delta_temp += (ancienne_temp - ctc(i, j).temp) *
-                        (ancienne_temp - ctc(i, j).temp);
-                }
-            }
-        }
-
-        return std::sqrt(delta_temp / (larg * haut));
     }
 
     /**
@@ -146,61 +139,52 @@ public:
     /**
      * Accès à une composante
      */
-    inline png_byte chaleur(std::size_t rangee, std::size_t colonne) const {
-        return at(rangee * larg + colonne).chal;
+    inline double chaleur(std::size_t rangee, std::size_t colonne) const {
+        return at(rangee * larg + colonne).chaleur;
     }
-    inline png_byte temperat(std::size_t rangee, std::size_t colonne) const {
-        return at(rangee * larg + colonne).temp;
+    inline double temperature(std::size_t rangee, std::size_t colonne) const {
+        return at(rangee * larg + colonne).temperature;
     }
-    inline png_byte conduction(std::size_t rangee, std::size_t colonne) const {
-        return at(rangee * larg + colonne).cond;
+    inline double conduction(std::size_t rangee, std::size_t colonne) const {
+        return at(rangee * larg + colonne).conduction;
     }
 
     inline std::size_t largeur() const { return larg; }
     inline std::size_t hauteur() const { return haut; }
 
+    double un_pas_de_temps() {
+        double delta_temp = 0.;
+
+        // Convergence accélérée si traité en damier (une couleur à la fois)
+        for (auto impair = 0; impair < 2; ++impair) {
+            // Laisser faire la marge de 1 pixel
+            for (auto i = 1; i < haut - 1; ++i) {
+                auto depart = (((i + 1) ^ impair) & 1);  // Damier
+
+                for (auto j = 1 + depart; j < larg - 1; j += 2) {
+                    auto conduct = conduction(i, j);
+                    auto ancienne_temp = temperature(i, j);
+                    auto nouvelle_temp = std::max(chaleur(i, j), (
+                        temperature(i - 1, j) +
+                        temperature(i, j - 1) +
+                        temperature(i, j + 1) +
+                        temperature(i + 1, j) ) / 4);
+
+                    ctc(i, j).temperature = conduct * nouvelle_temp +
+                        (1.0 - conduct) * ancienne_temp;
+                    delta_temp += std::abs(
+                        ancienne_temp - ctc(i, j).temperature);
+                }
+            }
+        }
+
+        return delta_temp / (larg * haut);
+    }
+
 private:
     std::size_t larg;
     std::size_t haut;
 };
-
-
-/**
- * Affiche un aperçu du modèle en ASCII
- * @param ostr   Objet std::ostream tel que std::cout
- * @param modele Le modèle chaleur-température-conduction
- * @return L'objet std::ostream pour des appels à la chaîne
- */
-std::ostream& operator<<(std::ostream & ostr, const ModeleCTC & modele)
-{
-    const char ascii[] = " .,!~:;+=xzXZ%#@";
-    const auto largeur = modele.largeur(), hauteur = modele.hauteur();
-    const auto sautH = modele.largeur() / 80;  // caractères de large
-    const auto sautV = sautH * 2;
-
-    ostr << "Taille du modèle: " << largeur << " x " << hauteur << std::endl;
-
-    // Pour chaque bloc complet de taille sautH x sautV
-    for (auto i = sautV + hauteur % sautV / 2; i < hauteur; i += sautV) {
-        for (auto j = sautH + largeur % sautH / 2; j < largeur; j += sautH) {
-            unsigned int cond_tot = 0;
-
-            // Calculer la moyenne des valeurs de conduction ( [0, 256[ )
-            for (auto ii = i - sautV; ii < i; ++ii) {
-                for (auto jj = j - sautH; jj < j; ++jj) {
-                    cond_tot += modele.temperat(ii, jj);
-                }
-            }
-
-            // Sélectionner un des 16 caractères ASCII (/16 = *16/256)
-            ostr << ascii[cond_tot / (sautV * sautH * 16)];
-        }
-
-        ostr << std::endl;
-    }
-
-    return ostr;
-}
 
 
 /**
@@ -217,8 +201,20 @@ int main(int argc, char** argv)
     }
 
     try {
-        png.init_lecture(argv[1]);
-        carte_gpu.initialiser(png);
+        // Charger l'image
+        std::string nom_fichier(argv[1]);
+        png.charger(nom_fichier);
+
+        // Tranformer les pixels RGB en triplets CTC
+        carte_gpu.redimensionner(png.largeur(), png.hauteur());
+        std::transform(png.cbegin(), png.cend(), carte_gpu.begin(),
+            [](const png_color & pixel) {
+                return CTC {
+                    (double)pixel.red,
+                    (double)pixel.green,
+                    (double)pixel.blue / 256
+                };
+            });
     }
     catch (const std::string message) {
         std::cerr << "Erreur: " << message << std::endl;
@@ -229,19 +225,37 @@ int main(int argc, char** argv)
     double delta_temp = SEUIL_CONVERGENCE + 1.;
     unsigned int nb_iter = 0;
 
-    while(delta_temp > SEUIL_CONVERGENCE && nb_iter < NB_MAX_ITER) {
+    while (delta_temp > SEUIL_CONVERGENCE && nb_iter < NB_MAX_ITER) {
         delta_temp = carte_gpu.un_pas_de_temps();
         nb_iter++;
     }
 
+    const auto minmax = std::minmax_element(
+        carte_gpu.cbegin(), carte_gpu.cend(),
+        [](const CTC & a, const CTC & b) {
+            return a.temperature < b.temperature;
+        });
     std::cout << "Itération #" << nb_iter
-        << ", ajustement moyen = " << delta_temp << std::endl;
+        << ", ajustement moyen = " << delta_temp
+        << ", t_min = " << minmax.first->temperature
+        << ", t_max = " << minmax.second->temperature
+        << std::endl;
+
+    // Tranformer les triplets CTC en pixels RGB
+    std::transform(carte_gpu.cbegin(), carte_gpu.cend(), png.begin(),
+        [](const CTC & ctc) {
+            return png_color {
+                (png_byte)ctc.chaleur,
+                (png_byte)ctc.temperature,
+                (png_byte)(ctc.conduction * 256)
+            };
+        });
 
     // Aperçu après simulation
-    std::cout << carte_gpu << std::endl;
+    std::cout << png << std::endl;
 
     try {
-        carte_gpu.enregistrer(png, "resultat.png");
+        png.enregistrer("resultat.png");
     }
     catch (const std::string message) {
         std::cerr << "Erreur: " << message << std::endl;
